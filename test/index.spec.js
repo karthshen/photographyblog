7770d2561e0f0dbff1b69824d47786a309ab4191
karthshen <karth-shen@hotmail.com>
2020-08-15 14:52:11 -0400

    testing

---
+++ test/index.spec.js
@@ -0,0 +1,16 @@
+import { validateFields } from '../src/components/Helpers/Validation';
+
+describe('My Test Suite', () => {
+    it('My Test Case', () => {
+        expect(true).toEqual(true);
+    });
+});
+
+describe('Validation Test Suite', () => {
+    it('it takes in a valid string name, returns true', () => {
+        // const validName = 'test name';
+        // let result = validateFields.validateName(validName);
+
+        // expect(result).toEqual(true);
+    });
+});
