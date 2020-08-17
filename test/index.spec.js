7f16e745db47e959d5706d9a99a554fcd43cc238
conelson4 <cody.nelson4@yahoo.com>
2020-08-17 09:50:44 -0700

    added other test, but still having enviornment issues

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,16 +1,38 @@
 import { validateFields } from '../src/components/Helpers/Validation';
 
-describe('My Test Suite', () => {
-    it('My Test Case', () => {
-        expect(true).toEqual(true);
-    });
-});
-
 describe('Validation Test Suite', () => {
     it('it takes in a valid string name, returns true', () => {
-        // const validName = 'test name';
-        // let result = validateFields.validateName(validName);
+        const validName = 'test name';
+        let result = validateFields.validateName(validName);
 
-        // expect(result).toEqual(true);
+        expect(result).toEqual(true);
+    });
+
+    it('takes a valid email, returns true', () => {
+        const validEmail = 'test@test.com';
+        let result = validateFields.validateEmail(validEmail);
+
+        expect(result).toEqual(true);
+    });
+
+    it('takes a valid phone, returns true', () => {
+        const validPhone = 310-549-7462;
+        let result = validateFields.validatePhone(validPhone);
+
+        expect(result).toEqual(true);
+    });
+
+    it('takes a valid date, returns true', () => {
+        const validDate = 10/10/3000;
+        let result = validateFields.validateDate(validDate);
+
+        expect(result).toEqual(true);
+    });
+
+    it('takes a valid zip, returns true', () => {
+        const validZip = 25941;
+        let result = validateFields.validateZip(validZip);
+
+        expect(result).toEqual(true);
     });
 });
